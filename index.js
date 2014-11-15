6342b9bb1b878fcfbfa362f802c72aa34d7e95e2
IvanRave <reyvart@hotmail.com>
2014-11-16 02:06:56 +0300

    feat(fileStorage): add US settings
    check id of buckets and encoders
    
    time: 0h:10

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/** 
+ * Storage settings
+ *    do not change pre, frame and other without changing in a Cloud
+ * @module
+ */
+
 module.exports = [{
   regionKey: 'eu-west-1',
   srcBucket: 'vmg-eu-west-1-src',
@@ -10,6 +16,16 @@ module.exports = [{
   coderSrcDstId: '1416091029975-vzqxit',
   coderSrcDstName: 'vmg-eu-west-1-src-dst'
 }, {
+  srcBucket: 'vmg-us-east-1-src',
+  preBucket: 'vmg-us-east-1-pre',
+  frameBucket: 'vmg-us-east-1-frame',
+  dstBucket: 'vmg-us-east-1-dst',
+  thmbBucket: 'vmg-us-east-1-thmb',
+  coderSrcPreId: '1415922535952-68cvpl',
+  coderSrcPreName: 'vmg-us-east-1-src-pre',
+  coderSrcDstId: '1415923467055-rb1vuq',
+  coderSrcDstName: 'vmg-us-east-1-src-dst'
+}, {
   regionKey: 'eu-west-1',
   srcBucket: 'vmg-dev-src',
   preBucket: 'vmg-dev-pre',
@@ -21,14 +37,3 @@ module.exports = [{
   coderSrcDstId: '1416053390848-rdva7t',
   coderSrcDstName: 'vmg-dev-src-dst'
 }];
-// do not change pre, frame and other without changing in a Cloud
-//  src: 'vmg-us-east-1-src',
-//  pre: 'vmg-us-east-1-pre',
-//  frame: 'vmg-us-east-1-frame',
-//  dst: 'vmg-us-east-1-dst',
-//  thmb: 'vmg-us-east-1-thmb',
-//  coderSrcPreId: '1415922535952-68cvpl',
-//  coderSrcPreName: 'vmg-us-east-1-src-pre',
-//  coderSrcDstId: '1415923467055-rb1vuq',
-//  coderSrcDstName: 'vmg-us-east-1-src-dst'
-// real in production
