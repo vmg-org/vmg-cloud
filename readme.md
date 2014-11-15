a6fa2191629a8865bca19969eabedbddb3726946
IvanRave <reyvart@hotmail.com>
2014-11-15 15:27:44 +0300

    feat(app): init; create dev cloud
    buckets and pipelines; move to public git repo
    
    time: 1h:20

---
+++ readme.md
@@ -0,0 +1,6 @@
+VMG cloud
+===
+
+File storage, transcoder jobs and other work with media files
+
+One storage - few buckets and pipelines. This project helps to generate data for array of storages. 
---
+++ storages.js
@@ -0,0 +1,33 @@
+module.exports = [{
+  regionKey: 'eu-west-1',
+  srcBucket: 'vmg-dev-src',
+  preBucket: 'vmg-dev-pre',
+  frameBucket: 'vmg-dev-frame',
+  dstBucket: 'vmg-dev-dst',
+  thmbBucket: 'vmg-dev-thmb',
+  coderSrcPreId: '1416054196767-dc6mvi',
+  coderSrcPreName: 'vmg-dev-src-pre',
+  coderSrcDstId: '1416053390848-rdva7t',
+  coderSrcDstName: 'vmg-dev-src-dst'
+}];
+//}, {
+//  regionKey: 'us-east-1',
+//  srcBucket: process.env.S3_BUCKET_NAME,
+//  //  pre: '',
+//  //  frame: 'vmg-us-east-1-frame',
+//  //  dst: '',
+//  //  thmb: '',
+//  coderSrcPreId: '1415922535952-68cvpl',
+//  coderSrcPreName: 'vmg-us-east-1-src-pre',
+//  coderSrcDstId: '',
+//  coderSrcDstName: ''
+// do not change pre, frame and other without changing in a Cloud
+//  src: 'vmg-us-east-1-src',
+//  pre: 'vmg-us-east-1-pre',
+//  frame: 'vmg-us-east-1-frame',
+//  dst: 'vmg-us-east-1-dst',
+//  thmb: 'vmg-us-east-1-thmb',
+//  coderSrcPreId: '1415922535952-68cvpl',
+//  coderSrcPreName: 'vmg-us-east-1-src-pre',
+//  coderSrcDstId: '1415923467055-rb1vuq',
+//  coderSrcDstName: 'vmg-us-east-1-src-dst'
